b61f4a4a1f6b404ab443e72cc3e7c5ff379a7e82
Konstantin Brovtsev <brovan@yandex.ru>
2023-06-09 19:32:55 +0700

    fix: delete redundant code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import {pages, onNavigate, enableNavigation} from "./const/router.js";
+import {enableNavigation} from "./const/router.js";
 import login from './templates/login.hbs';
 import {props} from '../src/const/props'
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('here');
     const root = document.querySelector('#app');
-    // root.style = styles;
-    // document.head.appendChild(styles);
-    console.log(props.login);
     root.innerHTML = login(props.login);
     console.log(root);
     enableNavigation();
