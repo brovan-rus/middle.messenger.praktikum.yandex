67bbefdfb4b40f23568b9efcaa1e8ab18824df41
Konstantin Brovtsev <brovan@yandex.ru>
2023-08-13 13:52:03 +0300

    feat: add modal window

---
+++ src/components/Modal/Modal.ts
@@ -0,0 +1,57 @@
+import Block from '../../utils/Block';
+import styles from './Modal.module.css';
+import { removeModalFromDome, renderModalToDome } from '../../utils/modal';
+import { assertIsDefined } from '../../utils/assertIsDefined';
+import { assertIsNonNullable } from '../../utils/assertIsNonNullable';
+
+class Modal extends Block {
+  constructor(Component: Block) {
+    super('div', {
+      [Component.constructor.name]: Component,
+      styles,
+    });
+
+    this.props.events = {
+      click: (e: Event) => {
+        if (e.target === e.currentTarget) {
+          this.close();
+        }
+        if ((e.target as Element).id === 'closeButton') {
+          this.close();
+        }
+      },
+    };
+  }
+
+  componentDidMount() {
+    const attr = {
+      class: styles.container,
+    };
+    this.renewAttributes(attr);
+  }
+
+  render() {
+    const template = `
+            <div class="{{styles.content}}">
+            <button id="closeButton" class="{{styles.closeButton}}"></button>
+                {{{${Object.keys(this.children)[0]}}}}
+           </div>
+        `;
+
+    return this.compile(template, this.props);
+  }
+
+  open() {
+    renderModalToDome(this.getContent());
+    this.dispatchComponentDidMount();
+  }
+
+  close() {
+    const modalDataId = this.element?.getAttribute('data-id');
+    assertIsDefined(modalDataId);
+    assertIsNonNullable(modalDataId);
+    removeModalFromDome(modalDataId);
+  }
+}
+
+export default Modal;
---
+++ src/utils/modal.ts
@@ -0,0 +1,18 @@
+export const renderModalToDome = (modal: Element) => {
+  const root = document.querySelector('body');
+  if (!root) {
+    throw new Error('invalid query');
+  }
+  root.append(modal);
+  return root;
+};
+
+export const removeModalFromDome = (modalDataId: string) => {
+  const root = document.querySelector('body');
+  const modalElement = document.querySelector(`[data-id="${modalDataId}"]`);
+  if (!root) {
+    throw new Error('invalid query');
+  }
+  modalElement?.remove();
+  return root;
+};
